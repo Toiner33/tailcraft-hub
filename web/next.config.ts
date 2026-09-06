f9de515e2f7ffb6cad63f86b3ca72f774c26de39
Toiner <adrian.trujillo.jp@gmail.com>
2026-09-06 21:51:37 +0900

    Allow connections other local devices

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+// Collect dev origins dynamically
+const devOrigins = ['localhost:3000', '100.*.*.*'];
+
+// If a private machine name is defined in .env.local, append it
+if (process.env.MY_MACHINE_NAME) {
+  devOrigins.push(process.env.MY_MACHINE_NAME);
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: devOrigins,
 };
 
 export default nextConfig;
